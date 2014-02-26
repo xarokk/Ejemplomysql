ec17fee32dc4ce6160085d5d2f7b372d1339c8e5
xarokk <xarokk@xarokk-PC>
2014-02-26 13:09:33 +0100

    ejemplobasededatos commit1

---
+++ src/main/java/edu/upc/eetac/dsa/cArtigas/Ejemplo1/App.java
@@ -0,0 +1,13 @@
+package edu.upc.eetac.dsa.cArtigas.Ejemplo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
